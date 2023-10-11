7b53d0e7b4814316e59640b859c0deb6533ca33a
Knoxx <37295154+Winston-Churchill@users.noreply.github.com>
2023-10-11 23:18:36 +0300

    Added functions to clear grid and allow drawing.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const gridContainer = document.querySelector(".grid-container");
 const slider = document.querySelector("#slider");
 const sliderNumber = document.querySelector("#slider-number");
+
 sliderNumber.textContent = slider.value;
 
 const clear = document.querySelector("#clear");
@@ -10,32 +11,42 @@ const clear = document.querySelector("#clear");
 
 function createGrid(dim)
 {
-    
-
     gridContainer.style.gridTemplateRows = `repeat(${dim},1fr)`;
     gridContainer.style.gridTemplateColumns = `repeat(${dim},1fr)`;
-
+    
+    
     for(let i=0;i<dim*dim;i++)
     {
         const square = document.createElement('div');
         
         square.id = i;
+        square.classList.add('grid-box');
         gridContainer.append(square);
     }
+
+    drawOnGrid();
+    clearGrid();
+    
 }
 
-createGrid(16);
 
-// function clearGrid()
-// {
-//     clear.addEventListener('click',()=>{
-       
-//     })
-// }
+
+function clearGrid()
+{
+    const gridboxes = document.querySelectorAll('.grid-box');
+    clear.addEventListener('click',()=>{
+       gridboxes.forEach( (box)=>{
+            box.style.backgroundColor = '#e5e7eb';
+       })
+    })
+}
 
 function updateGrid(newSize)
 {
     sliderNumber.textContent = newSize;
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
     createGrid(newSize);
 }
 
@@ -53,6 +64,33 @@ function handleSliderEvents()
 }
 
 
-// clearGrid();
+function drawOnGrid()
+{
+    //Boolean that lets us handle when a user can draw
+    let isDrawing = false;
+    const gridboxes = document.querySelectorAll('.grid-box');
+     // If mouse click is pressed down someone can draw
+    gridContainer.addEventListener('mousedown',(e)=>{
+        e.preventDefault();
+        isDrawing = true;
+    });
+    //Once click is released, stop drawing
+    gridContainer.addEventListener('mouseup',()=>{
+        isDrawing = false;
+    });
+
+    //Check if a person moves the mouse and is holding the mouse down then enable drawing on the gridboxes
+    gridContainer.addEventListener('mousemove', (e) => {
+        if (isDrawing && e.target.classList.contains('grid-box')) {
+            e.target.style.backgroundColor = 'black';
+        }
+    });
+  
+    
+}
+
+createGrid(16);
 handleSliderEvents();
+
+
 
