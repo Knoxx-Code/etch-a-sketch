10ec31d707fa8bd072ddcc6b8a6f26f3d9b7f33d
Knoxx <37295154+Winston-Churchill@users.noreply.github.com>
2023-10-11 20:27:17 +0300

    Added slider to change grid size.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 const gridContainer = document.querySelector(".grid-container");
 const slider = document.querySelector("#slider");
 const sliderNumber = document.querySelector("#slider-number");
+sliderNumber.textContent = slider.value;
 
+const clear = document.querySelector("#clear");
 
 
 function createGrid(dim)
@@ -16,11 +18,41 @@ function createGrid(dim)
     for(let i=0;i<dim*dim;i++)
     {
         const square = document.createElement('div');
-        square.style.backgroundColor = 'red';
+        
         square.id = i;
         gridContainer.append(square);
     }
 }
 
 createGrid(16);
+
+// function clearGrid()
+// {
+//     clear.addEventListener('click',()=>{
+       
+//     })
+// }
+
+function updateGrid(newSize)
+{
+    sliderNumber.textContent = newSize;
+    createGrid(newSize);
+}
+
+function handleSliderEvents()
+{
+    slider.addEventListener('input',(e)=>{
+        let newSize = e.target.value;
+        updateGrid(newSize);
+    })
+
+    slider.addEventListener('change',(e)=>{
+        let newSize = e.target.value;
+        updateGrid(newSize);
+    })
+}
+
+
+// clearGrid();
+handleSliderEvents();
 
