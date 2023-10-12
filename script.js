c27bc2d59c4e0aba182b4abf869c59d755a26966
Knoxx <37295154+Winston-Churchill@users.noreply.github.com>
2023-10-12 23:44:06 +0300

    Added buttons that can change the colour and erase

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 
-
+//Grid container
 const gridContainer = document.querySelector(".grid-container");
+//Slider elements
 const slider = document.querySelector("#slider");
 const sliderNumber = document.querySelector("#slider-number");
-
 sliderNumber.textContent = slider.value;
-
+//Clear button
 const clear = document.querySelector("#clear");
 
+//Global variable to help us randomize the colors
+let isRandom = false;
 
-function createGrid(dim)
+//Color picker buttons
+const blackButton = document.querySelector("#black-button");
+const multiColorButton = document.querySelector("#multi-color-button");
+const eraser = document.querySelector("#eraser");
+const colorPicker = document.querySelector("#color-picker");
+
+
+
+function manipulateGrid(dim)
 {
     gridContainer.style.gridTemplateRows = `repeat(${dim},1fr)`;
     gridContainer.style.gridTemplateColumns = `repeat(${dim},1fr)`;
@@ -28,8 +38,6 @@ function createGrid(dim)
     clearGrid();
     
 }
-
-
 
 function clearGrid()
 {
@@ -63,9 +71,76 @@ function handleSliderEvents()
     })
 }
 
+function getRandomColor()
+{
+    //Letters that make up color code
+    let chars = '0123456789ABCDEF';
+    //Starting symbol in a color code
+    let color = '#';
+
+    //Loop through the characters 6 times(length of color code) and get random characters
+    for(let i=0;i<6;i++)
+    {
+        color+= chars[Math.floor(Math.random()*16)];
+    }
+
+    return color;
+}
+
+//Array that stores the buttons and color picker
+const buttons = [blackButton,multiColorButton,eraser,colorPicker];
+//Default color is black
+let currentColor = 'black';
+
+
+function handleActiveButton(activeButton)
+{
+    
+    //Remove the on click listener of active button to allow someone to click 
+    buttons.forEach((button)=>{
+        if(button!==activeButton)
+        {
+            button.removeEventListener('click',handleColors);
+            button.removeEventListener('input',handleColors);
+        }
+    })
+}
+
+function handleColors()
+{
+    // Current color is set to black
+    blackButton.addEventListener('click',()=>{
+        isRandom = false;
+        currentColor = 'black';
+        handleActiveButton(blackButton);
+    });
+
+    //Colorful button pressed, is random set to true meaning the colors will be ransomized
+    multiColorButton.addEventListener('click',()=>{
+        isRandom = true;
+        handleActiveButton(multiColorButton);
+    });
+
+    //Eraser allows us to 'erase' colored pixels by selecting the background color of our grid
+    eraser.addEventListener('click',()=>{
+        isRandom = false;
+        currentColor = '#e5e7eb';
+        handleActiveButton(eraser);
+    })
+
+    //Color picker allows a user to choose whick colour they would like to draw with
+    colorPicker.addEventListener('input',(e)=>{
+        isRandom = false;
+        currentColor = e.target.value;
+        handleActiveButton(colorPicker);
+    })
+
+}
 
 function drawOnGrid()
 {
+    //Function call for the function that handles color selection
+    handleColors();
     //Boolean that lets us handle when a user can draw
     let isDrawing = false;
     const gridboxes = document.querySelectorAll('.grid-box');
@@ -82,14 +157,22 @@ function drawOnGrid()
     //Check if a person moves the mouse and is holding the mouse down then enable drawing on the gridboxes
     gridContainer.addEventListener('mousemove', (e) => {
         if (isDrawing && e.target.classList.contains('grid-box')) {
-            e.target.style.backgroundColor = 'black';
+            //If isRandom is true each tile will be a random color
+            if (isRandom)
+            {
+                e.target.style.backgroundColor = getRandomColor();
+            }
+            else
+            {
+                e.target.style.backgroundColor = currentColor;
+            }
+            
         }
     });
-  
-    
+   
 }
 
-createGrid(16);
+manipulateGrid(16);
 handleSliderEvents();
 
 
